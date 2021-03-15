847fbbbc31584ea7e592018963a8cfa3738e0ad2
iberatkaya <beratkaya1998@gmail.com>
2021-03-15 13:26:48 +0300

    Changed BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
@@ -8,11 +8,11 @@ import i18n from "./translation";
 import * as serviceWorker from "./serviceWorker";
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <I18nextProvider i18n={i18n}>
       <Router />
     </I18nextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 ReactDOM.render(<App />, document.getElementById("root"));
