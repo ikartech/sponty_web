7023c2e4a2e4887245dfd0a3bd91bfa599bfe192
iberatkaya <beratkaya1998@gmail.com>
2021-03-22 23:41:15 +0300

    Removed unused imports

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,7 @@
 import { lazy } from "react";
 
 import IntroContent from "../../content/IntroContent.json";
-import MiddleBlockContent from "../../content/MiddleBlockContent.json";
-import AboutContent from "../../content/AboutContent.json";
-import MissionContent from "../../content/MissionContent.json";
-import ProductContent from "../../content/ProductContent.json";
-import ContactContent from "../../content/ContactContent.json";
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect";
+import { BrowserView, MobileView } from "react-device-detect";
 import { shuffleArray } from "../../common/util/util";
 import { Col, Row } from "antd";
 import Fade from "react-reveal/Fade";
